49e028ba77236c0710ee0806028d7b020ceae9fa
GuptaOum <oumgupta555@gmail.com>
2026-09-21 03:59:08 +0530

    Fix PEFT target_modules

--- train_lunar.py
+++ train_lunar.py
@@ -91,7 +91,7 @@ def get_model():
         config = LoraConfig(
             r=16, 
             lora_alpha=16, 
-            target_modules=["query", "value"], 
+            target_modules="all-linear", 
             lora_dropout=0.1, 
             bias="none", 
             modules_to_save=["classifier"]
