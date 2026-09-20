5bea98a66880c7ffe3a4228ba8bfbd709cf1ecae
GuptaOum <oumgupta555@gmail.com>
2026-09-21 04:13:57 +0530

    Disable cuDNN to fix sublibrary loading

--- train_lunar.py
+++ train_lunar.py
@@ -10,7 +10,10 @@ from tqdm import tqdm
 import numpy as np
 from sklearn.metrics import balanced_accuracy_score
 
+torch.backends.cudnn.enabled = False
+
 # Try importing PEFT and Transformers for LoRA + Google ViT. 
+
 # If not available, instructions will guide the user to pip install them.
 try:
     from transformers import ViTForImageClassification, ViTConfig
